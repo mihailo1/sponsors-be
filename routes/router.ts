5b6c54cc3b3659024d891c12d99934b94962e4e5
Misha Nikiforov <mikhail@joinsmiler.com>
2025-01-26 14:06:47 +0100

    api test

--- routes/router.ts
+++ routes/router.ts
@@ -47,7 +47,7 @@ router.get("/admin(.*)", async (context) => {
     try {
         console.log("Serving admin page");
 
-        Deno.env.set("REACT_APP_API_BASE_URL", `http://localhost:${Deno.env.get("PORT")}`);
+        Deno.env.set("REACT_APP_API_BASE_URL", `https://michael-sponsors-be-84.deno.dev`);
         await send(context, "index.html", {
             root: `${Deno.cwd()}/react-app/build`,
         });
@@ -61,6 +61,7 @@ router.get("/admin(.*)", async (context) => {
 // TODO: why /admin(.*) only works with .* method ?
 router.all("(.*)", async (context) => {
     try {
+        Deno.env.set("REACT_APP_API_BASE_URL", `https://michael-sponsors-be-84.deno.dev`);
         await send(context, context.request.url.pathname, {
             root: `${Deno.cwd()}/react-app/build`,
         });
